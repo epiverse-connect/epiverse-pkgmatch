151515bbfaaca8c523be7309387f2181a1d21624
Hugo Gruson <10783929+Bisaloo@users.noreply.github.com>
2024-11-21 17:42:15 +0100

    Add plumber API

---
+++ plumber.R
@@ -0,0 +1,17 @@
+#* @param query The query to search for
+#* @get /search
+#* @serializer json
+function(query) {
+  matches <- pkgmatch::pkgmatch_similar_pkgs(
+    input = query,
+    embeddings = pkgmatch.epi::epi_embeddings
+  )
+  matches$relevance <- 1 / matches$rank # FIXME: use better function
+  matches <- matches[, colnames(matches) != "rank"]
+
+  list(
+    query = query,
+    filter = "epiverse-connect",
+    response = list(results = purrr::transpose(matches))
+  )
+}
